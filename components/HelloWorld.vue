831c02978017a4a3ca93fdf43a958e1646d51b31
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-02 19:14:43 +0900

    Add show error.

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
     <header class="QiitaApp-header">
+        <font color="red"><b>{{error}}</b></font><br />
         <p>Nuxt.js 2, PWA</p>
         <a href="https://mbp.hatenablog.com/entry/2022/07/13/234924" target="_blank" rel="noreferrer" >MacでNuxt 3、VercelでNuxt3 App、QiitaAPIで記事情報を取得して表示(vercel-nuxt3_)</a><br />
         <button @click="getQiitaData()">Nuxt.js</button>
@@ -45,6 +46,7 @@ export default {
             page: 0,
             allQiitaData: [],
             isLoading: false,
+            error: "",
             //hello: "",
         }
     },

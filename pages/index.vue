36eeceb07f7ecd680e3d1a0b749bfb25111cd476
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-14 23:07:37 +0900

    Move HelloWorld.vue Qiita.vue.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <HelloWorld msg="Hello Nuxt2" />
+  <Home msg="Hello Nuxt2" />
 </template>
 
 <script setup>
-import HelloWorld from '../components/HelloWorld.vue'
+//import Home from '../components/Tutorial.vue'
+import Home from '../components/Qiita.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
